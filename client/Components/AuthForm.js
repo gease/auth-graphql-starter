bba1ec1d5445a446920a4334b187c2877d168ed0
gease <gease@mail.ru>
2022-08-25 12:56:46 +0200

    Authentication form errors handling.

--- client/Components/AuthForm.js
+++ client/Components/AuthForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 import {useMutation} from "@apollo/client";
 
 
@@ -9,15 +9,26 @@ import {CURRENT_USER} from "../queries";
 const AuthForm = ({mutation, button}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    const [mutate, {data, loading, error}] = useMutation(mutation);
-
+    const [mutate, {data, loading, error}] = useMutation(mutation, {refetchQueries: [CURRENT_USER]});
+    let errorsDisplayed = '';
+    if (error) {
+        if (error.graphQLErrors) {
+            errorsDisplayed = <ul className="card-content white-text">{error.graphQLErrors.map(item => <li>{item.message}</li>)}</ul>;
+        }
+        if (error.networkError) {
+            errorsDisplayed = <div className="card-content white-text">{error.networkError.message}</div>;
+        }
+    }
+    if (data && data.login)  {
+        return <Navigate to='/' />
+    }
     const onSubmit = (event) => {
         event.preventDefault();
-        mutate({variables:{email, password}, refetchQueries: [CURRENT_USER]}).then(() => navigate('/'));
+        mutate({variables:{email, password}});
     }
 
     return <form onSubmit={onSubmit}>
+        {errorsDisplayed ? <div className='card red'>{errorsDisplayed}</div> : ''}
         <input name='email' placeholder='you@somewhere.com' value={email} onChange={(event) => setEmail(event.target.value)} />
         <input type='password' placeholder='Something you will forget' name='password' value={password} onChange={(event) => setPassword(event.target.value)}/>
         <button>{button}</button>
